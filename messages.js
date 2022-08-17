45867911561e0aff429bea20c530e8248351f6be
lauraut <105700560+lauraut@users.noreply.github.com>
2022-08-17 07:34:19 +0100

    Added messages object

--- messages.js
+++ messages.js
@@ -1,1 +1,6 @@
 console.log("hola");
+var messages = {
+    first: ["Life's great journeys are ", "Innovation distinguishes between ", "Your time is limited, so ", "Doing the best at this moment ", "When you undervalue what you do, ", "Don't let the noise of others' opinions" ],
+    second: ["between a leader and a follower.", "don't waste it living someone else's life.", "drown out your own inner voice.", "the world will undervalue who you are.", "puts you in the best place for the next moment.", "Turn your wounds into wisdom."],
+    author: ["Steve Trabajos", "Opera Winnie", "Materia"]
+};
